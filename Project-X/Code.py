b8416aa176a79f0fb8542b727451d01c069d433a
Anurag <sn.anurag@gmail.com>
2018-07-15 11:21:50 +0530

    linear regression model

--- Project-X/Code.py
+++ Project-X/Code.py
@@ -2,47 +2,56 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import SGD
+from sklearn.model_selection import train_test_split
 
 # Generate dummy data
 import numpy as np
 import pandas as pd
 
-# i_train = pd.read_csv('/Users/anurag/Documents/workspace/kaggle/Home_Credit_Default_Risk/Data/application_train.csv', index_col=[0,1]).as_matrix() 
-# o_train = pd.read_csv('/Users/anurag/Documents/workspace/kaggle/Home_Credit_Default_Risk/Data/application_train.csv', usecols=[1]).as_matrix()
-# i_test =  pd.read_csv('/Users/anurag/Documents/workspace/kaggle/Home_Credit_Default_Risk/Data/application_test.csv', index_col=[0]).as_matrix() 
-cat1 = pd.read_csv('/Users/anurag/Documents/workspace/kaggle/Home_Credit_Default_Risk/Data/test.csv', index_col=[0]).as_matrix()
-df = pd.DataFrame(cat1)
-print(df)
-df1 = df.apply(lambda x: pd.factorize(x)[0])
-print(df1)
-# cat2 = df1
-# print(cat2)
-cat3 = pd.read_csv('/Users/anurag/Documents/workspace/kaggle/Home_Credit_Default_Risk/Data/test2.csv', index_col=[0]).as_matrix()
-df2 = pd.DataFrame(cat3)
-print(df2.apply(lambda x: pd.factorize(x)[0]))
+# i_train = pd.read_csv('/Users/anurag/Documents/workspace/ML/Project-X/Data/test.csv', index_col=[0,1], dtype={'NAME_CONTRACT_TYPE': "category", 'CODE_GENDER': "category", 
+# 'FLAG_OWN_CAR': "category",	'FLAG_OWN_REALTY': "category",   'NAME_TYPE_SUITE': "category",	'NAME_INCOME_TYPE': "category",	'NAME_EDUCATION_TYPE': "category",
+# 	'NAME_FAMILY_STATUS': "category",	'NAME_HOUSING_TYPE': "category",    'OCCUPATION_TYPE': "category",   'WEEKDAY_APPR_PROCESS_START': "category",
+#   'ORGANIZATION_TYPE': "category",  'FONDKAPREMONT_MODE': "category",	'HOUSETYPE_MODE': "category",  'WALLSMATERIAL_MODE': "category",	'EMERGENCYSTATE_MODE': "category"})
+# o_train = pd.read_csv('/Users/anurag/Documents/workspace/ML/Project-X/Data/test.csv', usecols=[1])
 
-# x_train = np.random.random((1000, 20))
-# y_train = keras.utils.to_categorical(np.random.randint(10, size=(1000, 1)), num_classes=10)
-# x_test = np.random.random((100, 20))
-# y_test = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
+i_train = pd.read_csv('/Users/anurag/Documents/workspace/ML/Project-X/Data/application_train.csv', index_col=[0,1])
+o_train = pd.read_csv('/Users/anurag/Documents/workspace/ML/Project-X/Data/application_train.csv', usecols=[1])
+i_test =  pd.read_csv('/Users/anurag/Documents/workspace/ML/Project-X/Data/application_test.csv', index_col=[0])
 
+full_frame = pd.concat([i_train, i_test])
+def fact(x): 
+    if x.dtype == np.object: return pd.factorize(x)[0] 
+    else: return x
+
+full_frame = full_frame.apply(fact)
+full_frame.fillna(0) #TODO Not a correct approach
+# print(pd.Series(df.values.T[1], dtype="category").unique)
+# # print(df)
+# df1 = df.apply(lambda x: pd.factorize(x)[0])
+# # print(df1)
+# # cat2 = df1
+# # print(cat2)
+# cat3 = pd.read_csv('/Users/anurag/Documents/workspace/ML/Project-X/Data/test2.csv', index_col=[0]).as_matrix()
+# df2 = pd.DataFrame(cat3)
+# # print(df2.apply(lambda x: pd.factorize(x)[0]))
+
+# X_train, X_test, y_train, y_test = train_test_split(i_train, o_train, test_size=0.2, random_state=7)
+tmp = np.split(full_frame,[307511]) 
+i_train = tmp[0]
+i_test = tmp[1]
 model = Sequential()
-# Dense(64) is a fully-connected layer with 64 hidden units.
-# in the first layer, you must specify the expected input data shape:
-# here, 20-dimensional vectors.
-model.add(Dense(1, activation='relu', input_dim=120))
-# model.add(Dropout(0.5))
-# model.add(Dense(64, activation='relu'))
-# model.add(Dropout(0.5))
-# model.add(Dense(10, activation='softmax'))
+model.add(Dense(1, input_shape=(120,)))
 
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='sparse_categorical_crossentropy',
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.1, nesterov=True)
+model.compile(loss='mean_squared_error',
               optimizer=sgd,
               metrics=['accuracy'])
 
-model.fit(i_train, o_train,
-          epochs=20,
-          batch_size=128)
-# model.predict()
-# score = model.evaluate(x_test, y_test, batch_size=128)
+# model.fit(X_train, y_train,
+#           epochs=10,
+#           batch_size=128)
+model.fit(i_train, o_train, epochs=10, batch_size=128)
+
+o_test = model.predict(i_test)
+# score = model.evaluate(X_test, y_test, batch_size=128)
+print(o_test)
